b98c70ec111b7ead73dc5c649704558b0de204ac
Trevor <alutiiq@comcast.net>
2020-07-22 09:33:43 -0700

    day one working

---
+++ src/Components/List.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react"
+
+class List extends Component{
+    constructor(){
+        super()
+
+        this.state = {
+            movies: []
+        }
+    }
+
+    render(){
+        return <h1>This is the List component</h1>
+    }
+}
+
+export default List
